7744d8807dd246d4aa83479108fc52dfdb647163
sampadatiwari30 <sampadatiwari54@gmail.com>
2026-06-27 18:53:34 +0530

    Move Firebase config to environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,7 @@ import firebase from 'firebase'
 
 const firebaseConfig = {
     // your firebase config
- /*  apiKey: "AIzaSyANmWtmIeHeQd8YswXMSu5n4ruLE3VOVsI",
-  authDomain: "discord-clone-f893e.firebaseapp.com",
-  projectId: "discord-clone-f893e",
-  storageBucket: "discord-clone-f893e.firebasestorage.app",
-  messagingSenderId: "721360652620",
-  appId: "1:721360652620:web:51f8e955368aaa030ae7e9", */
-  // measurementId: "G-F3QSQ0HVF8"
+ 
   
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
